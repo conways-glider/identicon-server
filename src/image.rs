1c41568eb7f87d300efb2cf2128d3d7aa77f5159
Nia Watts <niawatts95@gmail.com>
2022-08-14 14:32:56 -0600

    Update to use axum

--- src/image.rs
+++ src/image.rs
@@ -1,43 +1,66 @@
-use actix_web::{get, web, HttpResponse, Responder};
+use std::path::Path as StdPath;
+
+use axum::http::{header, StatusCode};
+use axum::response::Response;
+use axum::{extract::Path, response::IntoResponse, Extension};
 use identicon_rs::Identicon;
 
+use crate::error::{self, AppError};
+use crate::Args;
 enum ImageType {
     Png,
     Jpeg,
 }
 
-fn generate_image(image_type: ImageType, path: web::Path<String>) -> impl Responder {
-    let identicon_string = path.into_inner();
-    let identicon = Identicon::new(&identicon_string);
+fn generate_image_response(
+    args: Args,
+    image_type: ImageType,
+    input: &str,
+) -> Result<Response, AppError> {
+    let mut identicon = Identicon::new(input);
+    identicon.set_border(args.border);
+    identicon.set_size(args.size).map_err(AppError::Identicon)?;
+    identicon
+        .set_scale(args.scale)
+        .map_err(AppError::Identicon)?;
+    identicon.set_mirrored(args.mirrored);
 
-    match image_type {
-        ImageType::Png => {
-            let data = identicon.export_png_data().unwrap();
-            HttpResponse::Ok().content_type("image/png").body(data)
+    let data = match image_type {
+        ImageType::Png => identicon.export_png_data().unwrap(),
+        ImageType::Jpeg => identicon.export_jpeg_data().unwrap(),
+    };
+    let headers = match image_type {
+        ImageType::Png => [(header::CONTENT_TYPE, "image/png".to_string())],
+        ImageType::Jpeg => [(header::CONTENT_TYPE, "image/jpeg".to_string())],
+    };
+    let response = (headers, data);
+    Ok(response.into_response())
+}
+
+pub(crate) async fn generate_image(
+    Path(name): Path<String>,
+    Extension(args): Extension<Args>,
+) -> Result<Response, AppError> {
+    let path = StdPath::new(name.as_str());
+    let name_extraction_error = error::new(
+        StatusCode::INTERNAL_SERVER_ERROR,
+        "Could not extract name from path",
+    );
+    match path.extension() {
+        Some(ext) if ext == "png" => {
+            let name = path.file_stem().and_then(|s| s.to_str());
+            match name {
+                Some(name) => generate_image_response(args, ImageType::Png, name),
+                None => Ok(name_extraction_error),
+            }
         }
-        ImageType::Jpeg => {
-            let data = identicon.export_jpeg_data().unwrap();
-            HttpResponse::Ok().content_type("image/jpeg").body(data)
+        Some(ext) if ext == "jpg" || ext == "jpeg" => {
+            let name = path.file_stem().and_then(|s| s.to_str());
+            match name {
+                Some(name) => generate_image_response(args, ImageType::Jpeg, name),
+                None => Ok(name_extraction_error),
+            }
         }
+        _ => generate_image_response(args, ImageType::Png, name.as_str()),
     }
-}
-
-#[get("/{name}")]
-pub async fn generate_png_raw_path(path: web::Path<String>) -> impl Responder {
-    generate_image(ImageType::Png, path)
-}
-
-#[get("/{name}.png")]
-pub async fn generate_png_path(path: web::Path<String>) -> impl Responder {
-    generate_image(ImageType::Png, path)
-}
-
-#[get("/{name}.jpg")]
-pub async fn generate_jpg_path(path: web::Path<String>) -> impl Responder {
-    generate_image(ImageType::Jpeg, path)
-}
-
-#[get("/{name}.jpeg")]
-pub async fn generate_jpeg_path(path: web::Path<String>) -> impl Responder {
-    generate_image(ImageType::Jpeg, path)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,48 @@
-use actix_web::{App, HttpServer};
+use std::net::SocketAddr;
 
+use axum::{routing::get, Extension, Router};
+use clap::Parser;
+
+mod error;
 mod image;
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(image::generate_jpeg_path)
-            .service(image::generate_jpg_path)
-            .service(image::generate_png_path)
-            .service(image::generate_png_raw_path)
-    })
-    .bind(("0.0.0.0", 8080))?
-    .run()
-    .await
+#[derive(Parser, Debug, Clone)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+    #[clap(long, value_parser, default_value_t = identicon_rs::Identicon::default().border())]
+    border: u32,
+
+    #[clap(long, value_parser, default_value_t = identicon_rs::Identicon::default().size())]
+    size: u32,
+
+    #[clap(long, value_parser, default_value_t = identicon_rs::Identicon::default().scale())]
+    scale: u32,
+
+    #[clap(long, value_parser, default_value_t = identicon_rs::Identicon::default().mirrored())]
+    mirrored: bool,
+}
+
+#[tokio::main]
+async fn main() {
+    identicon_rs::Identicon::default().border();
+    tracing_subscriber::fmt::init();
+    let args = Args::parse();
+
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/:name", get(image::generate_image))
+        .route("/", get(root))
+        .layer(Extension(args));
+    // `POST /users` goes to `create_user`
+    // .route("/users", post(create_user));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
 }
