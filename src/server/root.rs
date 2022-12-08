71fbb13a0f8fbce3e3e7f8ee56888a96b08802c6
Nia Watts <niawatts95@gmail.com>
2022-12-07 19:30:49 -0700

    Remove unused comment

--- src/server/root.rs
+++ src/server/root.rs
@@ -4,8 +4,6 @@ use tracing::instrument;
 use crate::config::AppState;
 
 pub fn router() -> Router<AppState> {
-    // By having each module responsible for setting up its own routing,
-    // it makes the root module a lot cleaner.
     Router::new().route("/", get(handler))
 }
 
