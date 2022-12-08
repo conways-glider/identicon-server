b9dd96f540e99633bb6626c7b613ddf611359528
Nia Watts <niawatts95@gmail.com>
2022-12-07 18:53:53 -0700

    Add middlewares and error handling

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,8 +1,9 @@
 use anyhow::Context;
-use axum::Router;
-use tracing::info;
-use std::net::SocketAddr;
-use tower_http::trace::TraceLayer;
+use axum::{error_handling::HandleErrorLayer, http::StatusCode, response::IntoResponse, Router};
+use std::{borrow::Cow, net::SocketAddr, time::Duration};
+use tower::{BoxError, ServiceBuilder};
+use tower_http::trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer};
+use tracing::{info, Level};
 
 mod root;
 mod signal;
@@ -23,9 +24,39 @@ pub async fn start_server() -> anyhow::Result<()> {
 }
 
 fn api_router() -> Router {
-    // This is the order that the modules were authored in.
+    // construct middleware
+    let trace_layer = TraceLayer::new_for_http()
+        .make_span_with(DefaultMakeSpan::default().level(Level::INFO))
+        .on_response(DefaultOnResponse::default().level(Level::INFO))
+        .on_request(DefaultOnRequest::default().level(Level::INFO));
+
+    let middleware_stack = ServiceBuilder::new()
+        .layer(HandleErrorLayer::new(handle_error))
+        .load_shed()
+        .concurrency_limit(1024)
+        .timeout(Duration::from_secs(5))
+        .layer(trace_layer);
+
     Router::new()
         .merge(root::router())
         // Enables logging. Use `RUST_LOG=tower_http=debug`
-        .layer(TraceLayer::new_for_http())
+        .layer(middleware_stack)
+}
+
+async fn handle_error(error: BoxError) -> impl IntoResponse {
+    if error.is::<tower::timeout::error::Elapsed>() {
+        return (StatusCode::REQUEST_TIMEOUT, Cow::from("request timed out"));
+    }
+
+    if error.is::<tower::load_shed::error::Overloaded>() {
+        return (
+            StatusCode::SERVICE_UNAVAILABLE,
+            Cow::from("service is overloaded, try again later"),
+        );
+    }
+
+    (
+        StatusCode::INTERNAL_SERVER_ERROR,
+        Cow::from(format!("Unhandled internal error: {}", error)),
+    )
 }
